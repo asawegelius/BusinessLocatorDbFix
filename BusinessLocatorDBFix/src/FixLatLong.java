69fdbd62b0fbf3506649acc2f5f77077f043b939
asawegelius <asa.wegelius@gmail.com>
2015-11-18 12:54:32 +0100

    working on adding lat long from old table

--- BusinessLocatorDBFix/src/FixLatLong.java
+++ BusinessLocatorDBFix/src/FixLatLong.java
@@ -1,4 +1,49 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class FixLatLong {
 
+	public ArrayList<String> getCompCodeFromCompLoc() {
+		ArrayList<String> codes = new ArrayList<String>();
+		String query = "select comp_code from comp_loc;";
+		Connection con = DBConnect.getConnection();
+		try {
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery(query);
+			if (rs != null) {
+				while (rs.next()) {
+					codes.add(rs.getString("comp_code"));
+				}
+			}
+			s.close();
+		} catch (SQLException e) {
+			System.err.println("in findByID " + e.getMessage());
+
+		}
+		return codes;
+	}
+
+	public ArrayList<Integer> getCompIdsFromCompanies(ArrayList<String> comp_codes) {
+		ArrayList<Integer> ids = new ArrayList<Integer>();
+		String query = "select company_id from company where company_code ='";
+		Connection con = DBConnect.getConnection();
+		try {
+			for (String code : comp_codes) {
+				Statement s = con.createStatement();
+				ResultSet rs = s.executeQuery(query + code + "';");
+				if (rs != null) {
+						System.out.println("getting an id");
+						ids.add(rs.getInt("company_id"));
+				}
+				s.close();
+			}
+		} catch (SQLException e) {
+			System.err.println("in findByID " + e.getMessage());
+
+		}
+		return ids;
+	}
 }
--- BusinessLocatorDBFix/src/Run.java
+++ BusinessLocatorDBFix/src/Run.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
 
 public class Run {
 
 	public static void main(String[] args) {
 		//new CsvToTable().fixCompanies();
-		new TestDb().getBusinessForms();
+		//new TestDb().getBusinessForms();
 		//new CsvToTable().fixAddresses();
 		//new CsvToTable().oldComps();
+		FixLatLong blob = new FixLatLong();
+		ArrayList<String> codes = blob.getCompCodeFromCompLoc();
+		System.out.println("no of codes = " + codes.size());
+		ArrayList<Integer> ids = blob.getCompIdsFromCompanies(codes);
+		System.out.println("no of ids = " + ids.size());
 	}
 
 }
